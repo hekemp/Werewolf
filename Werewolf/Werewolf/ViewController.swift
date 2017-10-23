c4f393220a7a9ae84da6e83670d43bab650bb4f2
macbook_user <macbook_user@cs-m102.local>
2017-10-23 10:41:35 -0500

    initial commit

---
+++ Werewolf/Werewolf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Werewolf
+//
+//  Created by macbook_user on 10/23/17.
+//  Copyright © 2017 CS4980-Werewolf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
